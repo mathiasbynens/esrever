f8b6bba98da33a98c60ebc4c1c50a0b477d9eac9
Mathias Bynens <mathias@qiwi.be>
2013-10-17 12:13:08 -0500

    src/data.js: Avoid hardcoding Unicode blocks

--- scripts/export-data.js
+++ scripts/export-data.js
@@ -8,17 +8,15 @@ var unicodePackage = Object.keys(packageInfo.devDependencies).filter(function(ke
 	return /^unicode-/.test(key);
 })[0];
 
-function getBlock(name) {
-	return require(unicodePackage + '/blocks/' + name + '/code-points');
-}
-
 // All types of combining marks
-var combiningMarks = regenerate(
-	getBlock('Combining Diacritical Marks'),
-	getBlock('Combining Diacritical Marks Supplement'),
-	getBlock('Combining Diacritical Marks for Symbols'),
-	getBlock('Combining Half Marks')
-);
+var combiningMarks = regenerate();
+require(unicodePackage).blocks.forEach(function(block) {
+	if (/^Combining/.test(block)) {
+		combiningMarks.add(
+			require(unicodePackage + '/blocks/' + block + '/code-points')
+		);
+	}
+});
 
 // All code points except those that map to combining marks
 var allExceptCombiningMarks = regenerate()
